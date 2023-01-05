a20dd1ff1afa49067cfc6e594df86b90e69614a8
Josiah S <jsch0049@student.monash.edu>
2023-01-05 22:31:24 +1100

    Added add song to queue functionality

--- controller.js
+++ controller.js
@@ -1,22 +1,58 @@
 
-function convertItemsToHTML(items) {
+async function convertItemsToHTML(items, auth) {
     /*
     Converts an object of items to HTML
     Inputs:
     - items (Object): Object of items from Spotify API
+    - auth (String): access-token for Spotify API
     Output: String of HTML to display the items
     */
     let types = ["tracks"]; // Update this when the scope of types is increased
     let output = ""
     for (let type in types) {
         for (let itemIndex in items[types[type]]["items"]) {
+            // Extract information from object
             let item = items[types[type]]["items"][itemIndex];
             let name = item["name"];
             let artists = item["artists"].map(artist => artist["name"]).join(", ");
-            output += `${name}, by ${artists}<br>`
+            let duration = convertMsToTime(item["duration_ms"]);
+
+            // Setup information for adding song to queue
+            let song_uri = item["uri"];
+            let device_id = await getDeviceId(auth);
+
+            // Construct HTML output
+            output += `
+            <a href="javascript:addToQueueWrapper('${song_uri}', '${device_id}', '${auth}')">
+            <div class="mdl-list__item">
+                <span class="mdl-list__item-primary-content">
+                    <span>${name}, by ${artists} (${duration})</span>
+                </span>
+                <i class="material-icons">add</i>
+            </div>
+            </a><br>`;
         }
     }
     return output;
+}
+
+async function addToQueueWrapper(songUri, device_id, auth) {
+    /*
+    Adds a song to the queue after checking if a device is active
+    - songUri (String): URI of song to be added to queue
+    - device_id (String): ID of device to be played on
+    - auth (String): access token for Spotify API
+    */
+    console.log(device_id);
+    // Check if a device is active
+    if (device_id != null && device_id != "null") {
+        // A device is active
+        console.log("Song added to queue");
+        await addToQueue(songUri, device_id, auth);
+    } else {
+        // No devices are active
+        console.log("No devices available");
+    }
 }
 
 async function search() {
@@ -49,7 +85,7 @@ async function search() {
 
         // Display the results in the HTML
         let outputRef = document.getElementById("output");
-        outputRef.innerHTML = convertItemsToHTML(searchOutput);
+        outputRef.innerHTML = await convertItemsToHTML(searchOutput, access_token);
     }
 }
 
--- util/apiUtil.js
+++ util/apiUtil.js
@@ -99,16 +99,33 @@ async function getNewAccessToken(refresh_token, client_id, client_secret) {
     return token;
 }
 
-async function searchSpotify(query, auth, type="track") {
+function convertMsToTime(ms) {
+    /*
+    Converts milliseconds to mm:ss time
+    Inputs:
+    - ms (Number): amount of milliseconds
+    Output: String of time in mm:ss format
+    */
+    let secondsLength = ms/1000
+    let minutes = Math.floor(secondsLength / 60);
+    let seconds = Math.round(secondsLength % 60);
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`;
+}
+
+async function searchSpotify(query, auth, limit=10, type="track") {
     /*
     Searches for an item with the Spotify API
     Inputs:
     - query (String): the search query
     - auth (String): the access token
+    - limit (Number): number of search results returned
     - type (String): the type of item
     Output: Object of data returned from API
     */
-    let url = `https://api.spotify.com/v1/search?q=${query}&type=${type}&include_external=audio`;
+    let url = `https://api.spotify.com/v1/search?q=${query}&limit=${limit}&type=${type}&include_external=audio`;
     
     let headers = {
         "Accept": "application/json",
@@ -120,4 +137,60 @@ async function searchSpotify(query, auth, type="track") {
     let data = await response.json();
 
     return data;
+}
+
+async function getDeviceId(auth) {
+    /*
+    Gets available devices to play on
+    Inputs:
+    - auth (String): access token for Spotify API
+    Output: String of device ID, or null if there are no devices
+    */
+    let device_id = null;
+
+    let url = "https://api.spotify.com/v1/me/player/devices";
+
+    let headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + auth
+    }
+    
+    let response = await fetch(url, {headers: headers});
+    let data = await response.json();
+
+    // Find the active device (device in use)
+    for (let i = 0; i < data["devices"].length; i++) {
+        let device = data["devices"][i];
+        if (device["is_active"]) {
+            device_id = device["id"];
+        }
+    }
+
+    // If there are devices but none are active, then return the first device
+    // if (device_id === null && data["devices"].length > 0) {
+    //     device_id = data["devices"][0]["id"];
+    // }
+
+    return device_id;
+}
+
+async function addToQueue(songUri, device_id, auth) {
+    /*
+    Adds a song to the queue
+    - songUri (String): URI of song to be added to queue
+    - device_id (String): ID of device to be played on
+    - auth (String): access token for Spotify API
+    */
+    let url = `https://api.spotify.com/v1/me/player/queue?uri=${songUri}&device_id=${device_id}`;
+    let headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + auth
+    }
+    
+    let response = await fetch(url, {headers: headers, method: "POST"});
+    let data = await response.json();
+
+    console.log(data);
 }
