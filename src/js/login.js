6f6ddd602fcb75d53e6131d82dd84f63369f130e
Josiah S <jsch0049@student.monash.edu>
2023-02-16 19:57:28 +1100

    Replaced client ID and client secret login with Spotify login

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,9 @@
 
 const HASHED_JOSIAH_PASSWORD = "YL1lGNH2jaDFVAPTNXp8Z/yoXpu/5xsbwrHASsbkZuQ=";
-const ENCRYPTED_JOSIAH_CLIENT_ID = "U2FsdGVkX1+Cw9T4nNXC9cJr5+OeygEPOw+Ulr5A9K0ERFaaGl5grQp4aiklMaHsL7vkq7azTMhOV4syasBPRQ==";
-const ENCRYPTED_JOSIAH_CLIENT_SECRET = "U2FsdGVkX189vxEJWKZ1FS7RLlk9wb13H5PAcprOt3PzbQgxWV0uu5rsrulfUCPVlrvDxw78EyhHuRkMI4yP1Q==";
 const ENCRYPTED_JOSIAH_REFRESH_TOKEN = "U2FsdGVkX1/5ZjwstEbch4D48Oa4ilkCN322OiC82M9gQmL7c9JE7O/S30GPOM81VbJ6o4A+fhFiXGP4iSG0Ci5C7HU/wsC4QEnUE5Hk02lu6nLfBh4QW/xdK2v2lTVM9WmTvVlMnVYLrLHVs3XVkwtg4zXVqmt2XSLqaaKkHdOMAAiLa2KRclLD6Qi3fZ8JPTucpTa/J5+1z6/E9+DDLQ==";
+
+const CLIENT_ID = "f7893b6abe0d4475ae601778a41d0142";
+const CLIENT_SECRET = "32a87f23276c4a888fde2c1ffaf7c681";
 
 async function onPageLoad() {
     /*
@@ -17,32 +18,38 @@ async function onPageLoad() {
         // Get code from URL
         let urlParams = new URLSearchParams(window.location.search);
         let code = urlParams.get("code");
+        
+        if (code === null) {
+            // If the user cancelled the log-in, go back to the log-in page
+            window.location.href = getUrlFromPage(window.location.href, "index.html");
+        } else {
+            // Successful log-in
+            // Clear URL
+            window.history.pushState("", "", getUrlFromPage(window.location.href, "index.html", "/index.html?code="));
+            let redirect_uri = window.location.href;
 
-        // Clear URL
-        window.history.pushState("", "", getUrlFromPage(window.location.href, "index.html", "/index.html?code="));
-        let redirect_uri = window.location.href;
+            // Get tokens
+            let tokens = await getTokens(code, client_id, client_secret, redirect_uri);
+            console.log(tokens);
 
-        // Get tokens
-        let tokens = await getTokens(code, client_id, client_secret, redirect_uri);
-        console.log(tokens);
+            // Save tokens to local storage
+            if (tokens["access_token"] != undefined){
+                localStorage.setItem("access_token", tokens["access_token"]);
+            }
+            if (tokens["refresh_token"] != undefined){
+                localStorage.setItem("refresh_token", tokens["refresh_token"]);
+            }
 
-        // Save tokens to local storage
-        if (tokens["access_token"] != undefined){
-            localStorage.setItem("access_token", tokens["access_token"]);
+            // Redirect to controller page
+            window.location.href = getUrlFromPage(window.location.href, "controller.html");
         }
-        if (tokens["refresh_token"] != undefined){
-            localStorage.setItem("refresh_token", tokens["refresh_token"]);
-        }
-
-        // Redirect to controller page
-        window.location.href = getUrlFromPage(window.location.href, "controller.html");
     } else if (localStorage.getItem("refresh_token") !== null) {
         // Automatically redirect to controller page if user has previously logged in
         window.location.href = getUrlFromPage(window.location.href, "controller.html");
     }
 }
 
-async function setLocalStorage(client_id, client_secret) {
+async function clientLogin() {
     /*
     Adds items to the local storage
     Inputs:
@@ -51,41 +58,14 @@ async function setLocalStorage(client_id, client_secret) {
     */
 
     // If a new user is added, clear keys from local storage
-    if (localStorage.getItem("client_id") !== client_id) {
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("refresh_token");
-    }
 
     // Put Client ID and Secret into local storage
-    localStorage.setItem("client_id", client_id);
-    localStorage.setItem("client_secret", client_secret);
+    localStorage.setItem("client_id", CLIENT_ID);
+    localStorage.setItem("client_secret", CLIENT_SECRET);
 
-    // Check if a refresh token exists in local storage
-    if (localStorage.getItem("refresh_token") !== null) {
-        // Get refresh token from local storage
-        let refresh_token = localStorage.getItem("refresh_token");
-        // Generate new access token
-        let access_token = await getNewAccessToken(refresh_token, client_id, client_secret);
-        localStorage.setItem("access_token", access_token);
-
-        // Redirect to controller page
-        window.location.href = getUrlFromPage(window.location.href, "controller.html");
-    } else {
-        // No refresh token, so authorization is required
-        let redirect_uri = window.location.href;
-        await authorise(client_id, redirect_uri);
-    }
-}
-
-async function clientLogin() {
-    /*
-    This function is called when the Login button for Client ID and Client Secret is pressed.
-    */
-    let clientIDBoxRef = document.getElementById("clientIDBox");
-    let clientSecretBoxRef = document.getElementById("clientSecretBox");
-
-    // Set up local storage
-    await setLocalStorage(clientIDBoxRef.value, clientSecretBoxRef.value);
+    // Execute the Spotify Authorization flow
+    let redirect_uri = window.location.href;
+    await authorise(CLIENT_ID, redirect_uri);
 }
 
 async function josiahLogin() {
@@ -102,16 +82,14 @@ async function josiahLogin() {
         // Correct password
 
         // Decrypt encrypted API keys, using the password as the AES key
-        let client_id = aesDecrypt(ENCRYPTED_JOSIAH_CLIENT_ID, passwordGuess);
-        let client_secret = aesDecrypt(ENCRYPTED_JOSIAH_CLIENT_SECRET, passwordGuess);
         let refresh_token = aesDecrypt(ENCRYPTED_JOSIAH_REFRESH_TOKEN, passwordGuess);
 
         // Get new access token
-        let access_token = await getNewAccessToken(refresh_token, client_id, client_secret);
+        let access_token = await getNewAccessToken(refresh_token, CLIENT_ID, CLIENT_SECRET);
 
         // Set up local storage
-        localStorage.setItem("client_id", client_id);
-        localStorage.setItem("client_secret", client_secret);
+        localStorage.setItem("client_id", CLIENT_ID);
+        localStorage.setItem("client_secret", CLIENT_SECRET);
         localStorage.setItem("refresh_token", refresh_token);
         localStorage.setItem("access_token", access_token);
 
